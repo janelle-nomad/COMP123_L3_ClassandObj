fe88d1b7fa0ccd11c665d2c8e6192b0d5451200f
Janelle Nomad <jheron3@my.centennialcollege.ca>
2016-05-24 14:20:06 -0400

    Updated person class and updated properties

--- COMP123_L3_ClassandObj/Person.cs
+++ COMP123_L3_ClassandObj/Person.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace COMP123_L3_ClassandObj
 {
+
+    /**
+     * This class defines a generic person
+     * 
+     * @class Person
+     * @field _name {string}
+     */
    public class Person
     {
+        // PRIVATE INSTANCE VARIABLES ***********************
+        // all private variable will have an underscore in the beginning*
 
+        private string _name;
+
+        // PUBLIC PROPERTIES ***********************
+        /**
+         *  This is a peroperty for our _name field
+         *  
+         *  @property {string} Name
+         */
+        public string Name
+       {
+           // getters get data 
+           // setters set the value of data inside the class
+
+           get { return this._name; }
+
+           // If i only have a getter it turns the data into a read only property
+
+           set { this._name = value; }
+       }
+        /**
+             * @ This is the default constructor for the person class
+             * 
+             * @ Constructor Person
+        */
+        public Person()
+        {
+            this._name = "Error: Unknown name!";
+        }
     }
+
 }
--- COMP123_L3_ClassandObj/Program.cs
+++ COMP123_L3_ClassandObj/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace COMP123_L3_ClassandObj
 {
-   public class Program
+
+    /**
+     * This class is the "driver" class of our program
+     */
+    public class Program
     {
+        /**
+         *  The main method for our driver class Program
+         *  
+         *  @method main
+         *  @param {string[]} args
+         */
        public static void Main(string[] args)
         {
+           // Creates a new object of person class, in other words breathes life
+           Person person = new Person();
+
+           //Set a value in the name property class
+            person.Name = "Jenny";
+
+           // get the Name value and print to console
+           Console.WriteLine(person.Name);
+           
+           
+             
         }
     }
 }
